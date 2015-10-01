1fe1f17151f050b0dbfd055d827f94392a3969d4
benjamin <benjamin@puppetlabs.vm>
2015-10-01 18:35:55 +0000

    Adding hiera

--- modules/nginx/manifests/params.pp
+++ modules/nginx/manifests/params.pp
@@ -1,10 +1,10 @@
 class nginx::params {
   case $::osfamily {
     'RedHat' : {
-      $nginx_doc_root = '/var/www'
-      $nginx_cfg      = '/etc/nginx/nginx.conf'
-      $nginx_default  = '/etc/nginx/conf.d/default.conf'
-      $index_file     = '/var/www/index.html'
+      $nginx_doc_root = hiera('nginx_doc_root')
+      $nginx_cfg      = hiera('nginx_cfg')
+      $nginx_default  = hiera('nginx_default')
+      $index_file     = hiera('index_file')
     }
     default: {
       fail("Not supported OS")
